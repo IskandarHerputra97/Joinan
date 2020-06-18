2968c4ae4853c938189e6d705f96ed3734ee7beb
iskandarherputra <iskandarherputra@gmail.com>
2020-06-18 10:18:12 +0700

    added tabbar controller for pelajaran vc, kelas saya vc, and profile vc

---
+++ Joinan/PelajaranViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PelajaranViewController.swift
+//  Joinan
+//
+//  Created by Iskandar Herputra Wahidiyat on 18/06/20.
+//  Copyright © 2020 Iskandar Herputra Wahidiyat. All rights reserved.
+//
+
+import UIKit
+
+class PelajaranViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Pelajaran"
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
